01c31989bacac039af82c44f7c1ebd4c8ceda0be
Alex Náger <51228484+aleexnager@users.noreply.github.com>
2022-10-11 20:38:36 +0200

    añado el resto de condiciones

--- pal_res.c
+++ pal_res.c
@@ -7,6 +7,22 @@ int es_pal_res(const char* lexema) {
         return BOOLEAN;
     } else if (strcmp(lexema, "for")) {
         return FOR;
+    } else if (strcmp(lexema, "function")) {
+        return FUNCTION;
+    } else if (strcmp(lexema, "if")) {
+        return IF;
+    } else if (strcmp(lexema, "input")) {
+        return INPUT;
+    } else if (strcmp(lexema, "int")) {
+        return INT;
+    } else if (strcmp(lexema, "let")) {
+        return LET;
+    } else if (strcmp(lexema, "print")) {
+        return PRINT;
+    } else if (strcmp(lexema, "return")) {
+        return RETURN;
+    } else if (strcmp(lexema, "string")) {
+        return STRING;
     } else {
         return -1;
     }
