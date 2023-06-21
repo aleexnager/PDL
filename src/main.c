e49c7ea068218dd8500fad2001b5f8499bbda85a
mciccale <marcociccalebaztan@gmail.com>
2023-06-21 15:24:22 +0200

    Implementado el acceso desde las funciones.

--- src/main.c
+++ src/main.c
@@ -7,6 +7,8 @@
 int id_tabla_global;
 const char *token_file_path = "./data/output/token.txt";
 const char *error_file_path = "./data/output/error.txt";
+const char *ts_file_path = "./data/output/ts.txt";
+const char *aux_ts_file_path = "./data/output/ts_aux.txt";
 
 int main(int argc, char *argv[])
 {
@@ -18,6 +20,8 @@ int main(int argc, char *argv[])
 
     remove(token_file_path);
     remove(error_file_path);
+    remove(ts_file_path);
+    remove(aux_ts_file_path);
 
     an_st(fp, id_tabla_global);
     destruir_tabla(id_tabla_global);
